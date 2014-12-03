eea402630246d1fad8c51cb054547eb7a68f192c
Maribel Fonseca <maribel@localhost.localdomain>
2014-12-03 10:49:51 -0600

    Reestructurando website - nuevos modelos y otros eliminados

--- website/admin.py
+++ website/admin.py
@@ -2,14 +2,24 @@
 from django.contrib import admin
 from website.models import * 
 
-admin.site.register(CategoriaInversion)
-admin.site.register(Inversion)
-admin.site.register(OrigenRecursos)
-admin.site.register(FuenteFmto)
-admin.site.register(Municipio)
-admin.site.register(Ingresos)
-admin.site.register(Area)
-admin.site.register(TipoGastos)
-admin.site.register(Gastos)
+class SubSubTipoIngresoInline(admin.TabularInline):
+    model = SubSubTipoIngreso
+    extra = 5
+class SubTipoIngresoAdmin(admin.ModelAdmin):
+    inlines = [SubSubTipoIngresoInline]
 
+admin.site.register(Departamento)
+admin.site.register(Municipio)
+admin.site.register(Comarca)
+admin.site.register(CatInversion)
+admin.site.register(TipoGasto)
+admin.site.register(SubTipoGasto)
+admin.site.register(OrigenRecurso)
+admin.site.register(TipoIngreso)
+admin.site.register(SubTipoIngreso, SubTipoIngresoAdmin)
+admin.site.register(FuenteFmto)
+admin.site.register(AreaGeografica)
+admin.site.register(Banner)
+admin.site.register(Donante)
+admin.site.register(TipoProyecto)
 
--- website/models.py
+++ website/models.py
@@ -1,70 +1,173 @@
 # -*- coding:utf-8 -*-
 from django.db import models
+from django.contrib.flatpages.models import FlatPage
+from sorl.thumbnail import ImageField
 
-class CategoriaInversion(models.Model):
-    nombre = models.CharField(max_length=30)
-
-    class Meta:
-        verbose_name_plural = 'Categoria Inversion'
- 
-class OrigenRecursos(models.Model):
-    nombre = models.CharField(max_length=30)
-
-    class Meta:
-        verbose_name_plural = 'Origen Recursos'
-
-class FuenteFmto(models.Model):
-    nombre = models.CharField(max_length=30)
-    origen_recurso = models.ForeignKey(OrigenRecursos, related_name="origen")
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=60)
+    latitud  = models.DecimalField('Latitud', max_digits=10, decimal_places=5, blank=True, null=True)
+    longitud = models.DecimalField('Longitud', max_digits=10, decimal_places=5, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Fuente Financiamiento'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
 
 class Municipio(models.Model):
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=120)
+    depto = models.ForeignKey(Departamento, related_name='departamento')
+    slug = models.SlugField(max_length=60)
     poblacion = models.IntegerField()
-    latitud  = models.DecimalField('Latitud', max_digits=8, decimal_places=5, blank=True, null=True)
-    longitud = models.DecimalField('Longitud', max_digits=8, decimal_places=5, blank=True, null=True)
-
-class Ingresos(models.Model):
-    municipio = models.ForeignKey(Municipio, related_name="municipio_ingreso") 
-    financiamiento = models.ForeignKey(FuenteFmto, related_name="fmto_ingreso")
+    latitud  = models.DecimalField('Latitud', max_digits=10, decimal_places=6, blank=True, null=True)
+    longitud = models.DecimalField('Longitud', max_digits=10, decimal_places=6, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Ingresos'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
 
-class Area(models.Model):
-    nombre = models.CharField(max_length=30)
+class Comarca(models.Model):
+    nombre = models.CharField(max_length=120)
+    municipio = models.ForeignKey(Municipio)
+    slug = models.SlugField(max_length=60)
+    poblacion = models.IntegerField()
+    latitud  = models.DecimalField('Latitud', max_digits=10, decimal_places=6, blank=True, null=True)
+    longitud = models.DecimalField('Longitud', max_digits=10, decimal_places=6, blank=True, null=True)
 
-class TipoGastos(models.Model):
-    nombre = models.CharField(max_length=30)
+    class Meta:
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
+
+class CatInversion(models.Model):
+    nombre = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=80)
+
+    class Meta:
+        verbose_name_plural = 'Categorias de inversion'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
+ 
+class TipoGasto(models.Model):
+    nombre = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=80)
  
     class Meta:
-        verbose_name_plural = 'Tipo Gastos'
+        verbose_name_plural = 'Tipo de gastos'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
 
-class Gastos(models.Model):
-    municipio = models.ForeignKey(Municipio, related_name="municipio_gasto")
-    area = models.ForeignKey(Area, related_name="area_gasto")
-    financiamieno = models.ForeignKey(FuenteFmto, related_name="fmto_gasto")
-    tipo_gasto = models.ForeignKey(TipoGastos, related_name="tipo_gasto")
-    fecha = models.DateField(null=True)
+class SubTipoGasto(models.Model):
+    tipogasto = models.ForeignKey(TipoGasto, related_name='tipo')
+    nombre = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=80)
+ 
+    class Meta:
+        verbose_name_plural = 'Sub-Tipo de gastos'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
+
+class OrigenRecurso(models.Model):
+    nombre = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=80)
 
     class Meta:
-        verbose_name_plural = 'Gastos'
+        verbose_name_plural = 'Origen  de los recursos'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
 
-class Inversion(models.Model):
-    municipio = models.ForeignKey(Municipio, related_name="municipio_inversion")
-    area = models.ForeignKey(Area, related_name="area_inversion")
-    categoria_inv = models.ForeignKey(CategoriaInversion, related_name="categoria")
-    financiamiento = models.ForeignKey(FuenteFmto, related_name="fmto_inv")
-    fecha = models.DateField(null=True)
+class TipoIngreso(models.Model):
+    CORRIENTE = 0
+    CAPITAL = 1
+    CLASIFICACION_CHOICES = (
+        (CORRIENTE, 'Ingreso Corriente'),
+        (CAPITAL, 'Ingreso Capital'),
+    )
+    nombre = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50)
+    #si no es ingreso corriente, entonces es de Capital
+    clasificacion = models.IntegerField(choices=CLASIFICACION_CHOICES, default=0)
+ 
+    class Meta:
+        verbose_name_plural = 'Tipo de ingreso'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
+
+class SubTipoIngreso(models.Model):
+    tipoingreso = models.ForeignKey(TipoIngreso, related_name='tipo')
+    nombre = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=80)
+ 
+    class Meta:
+        verbose_name_plural = 'Subtipos de ingresos'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
+
+class SubSubTipoIngreso(models.Model):
+    subtipoingreso = models.ForeignKey(SubTipoIngreso, related_name='tipo')
+    nombre = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=80)
+ 
+    class Meta:
+        verbose_name_plural = 'Sub-subtipos de ingresos'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
+
+class FuenteFmto(models.Model):
+    nombre = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=80)
 
     class Meta:
-        verbose_name_plural = 'Inversion'
+        verbose_name_plural = 'Fuentes de financiamiento'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
 
-class Presupuesto(models.Model):
-    municipio = models.ForeignKey(Municipio, related_name="municipio_presupuesto")
-    monto = models.IntegerField()
-    fecha = models.DateField()
-    descripcon = models.TextField(blank=True,null=True)
+class AreaGeografica(models.Model):
+    nombre = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=30)
 
+    class Meta:
+        verbose_name_plural = 'Area geografica'
+    def __unicode__(self):
+        return self.nombre
+
+class Banner(models.Model):
+    municipio = models.ForeignKey(Municipio)
+    titulo = models.CharField(max_length=200)
+    imagen = ImageField(upload_to='banner', verbose_name='Banner')
+    descripcion = models.CharField(max_length=500, null=True, blank=True)
+    enlace = models.CharField(max_length=200, null=True, blank=True)
+    orden = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['orden']
+    def __unicode__(self):
+        return self.titulo
+
+class Donante(models.Model):
+    nombre = models.CharField(max_length=200)
+    logo = ImageField(upload_to='donante', verbose_name='Logotipo')
+    descripcion = models.CharField(max_length=500, null=True, blank=True)
+    enlace = models.CharField(max_length=200, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.nombre
+
+class TipoProyecto(models.Model):
+    nombre = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=80)
+ 
+    class Meta:
+        verbose_name_plural = 'Tipo de proyectos'
+        ordering = ['nombre']
+    def __unicode__(self):
+        return self.nombre
