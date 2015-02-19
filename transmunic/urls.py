41af47822e68e28913a4e78a755251fe780eb07e
root <root@karinka.guegue.com>
2015-02-19 15:52:13 -0600

    Agregando url home view

--- transmunic/urls.py
+++ transmunic/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'core.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^chaining/', include('pixelfields_smart_selects.urls')),
 )
