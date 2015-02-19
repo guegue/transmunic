0b7a264a1c7b90c96b9bdbb228fae70d1668a62c
root <root@karinka.guegue.com>
2015-02-19 10:54:23 -0600

    Agregando list_display a municipios

--- lugar/admin.py
+++ lugar/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from lugar.models import * 
 
 # Register your models here.
+class MunicipioAdmin(admin.ModelAdmin):
+    list_display = ['id','depto','nombre']
+
 admin.site.register(Departamento)
-admin.site.register(Municipio)
+admin.site.register(Municipio,MunicipioAdmin)
 admin.site.register(Comarca)
