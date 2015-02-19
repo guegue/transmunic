1a55ce1f8b361bbb807eebfbd2c060fa66d9a701
root <root@karinka.guegue.com>
2015-02-19 15:50:09 -0600

    Home view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 # Create your views here.
+def home(request):
+    template_name = 'index.html'
+    return render_to_response(template_name, {
+    })
+
