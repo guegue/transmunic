2284ed4969e4d11e02c405d83f1afbd9fc14197b
Javier Wilson <javier.wilson@gmail.com>
2015-03-06 11:33:25 -0600

    oim barra comparativa, habilita filtro por municipio

--- core/charts.py
+++ core/charts.py
@@ -459,6 +459,10 @@ def oim_chart(municipio=None, year=None):
     if municipio:
         source = IngresoDetalle.objects.filter(ingreso__municipio__slug=municipio, ingreso__fecha=periodo).values('subsubtipoingreso__origen').annotate(ejecutado=Sum('ejecutado'), asignado=Sum('asignado')).order_by('subsubtipoingreso__origen')
         source_barra = IngresoDetalle.objects.filter(ingreso__fecha__in=periodos, ingreso__municipio__slug=municipio)
+        q = Q()
+        for y in list(year_list)[-3:]:
+            q |= Q(ingreso__fecha__year=y.year)
+        source_barra2 = IngresoDetalle.objects.filter(q, ingreso__municipio__slug=municipio)
     else:
         source = IngresoDetalle.objects.filter(ingreso__fecha=periodo).values('subsubtipoingreso__origen').annotate(ejecutado=Sum('ejecutado'), asignado=Sum('asignado')).order_by('subsubtipoingreso__origen')
         source_barra = IngresoDetalle.objects.filter(ingreso__fecha__in=periodos)
