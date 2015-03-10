401783e4dd0eb061ee128d5e0de8612aec27c593
root <root@karinka.guegue.com>
2015-03-10 15:18:44 -0600

    Agregando poblacion para municipios

--- lugar/admin.py
+++ lugar/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from lugar.models import * 
 
 # Register your models here.
+class PoblacionInline(admin.TabularInline):
+    model = Poblacion
+    extra = 5
+
 class MunicipioAdmin(admin.ModelAdmin):
     list_display = ['id','depto','nombre']
+    inlines = [PoblacionInline]
+    list_filter = ['depto']
 
 admin.site.register(Departamento)
 admin.site.register(Municipio,MunicipioAdmin)
