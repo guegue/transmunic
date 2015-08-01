c0bafd623eedb502251159882d9241844c7eb6fc
Javier Wilson <javier.wilson@gmail.com>
2015-08-01 09:43:44 -0600

    comenzando con ago

--- core/charts/aci.sql
+++ core/charts/aci.sql
@@ -4,42 +4,42 @@ SELECT ROW_NUMBER() OVER(ORDER BY anio) AS id, periodo.anio,
         ( SELECT sum(core_ingresodetalle.ejecutado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0)
         - 
         ( SELECT sum(core_gastodetalle.ejecutado) AS sum
            FROM core_gastodetalle
              JOIN core_gasto ON core_gastodetalle.gasto_id = core_gasto.id
-             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id::text = core_tipogasto.codigo::text
+             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id = core_tipogasto.codigo
           WHERE core_gasto.anio = periodo.anio AND core_tipogasto.clasificacion = 0)
         )
         / 
         ( SELECT sum(core_ingresodetalle.ejecutado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0)
-    ) * 100::numeric AS ejecutado,
+    ) * 100 AS ejecutado,
     (
         (
         ( SELECT sum(core_ingresodetalle.asignado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0)
         - 
         ( SELECT sum(core_gastodetalle.asignado) AS sum
            FROM core_gastodetalle
              JOIN core_gasto ON core_gastodetalle.gasto_id = core_gasto.id
-             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id::text = core_tipogasto.codigo::text
+             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id = core_tipogasto.codigo
           WHERE core_gasto.anio = periodo.anio AND core_tipogasto.clasificacion = 0)
         )
         / 
         ( SELECT sum(core_ingresodetalle.asignado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0)
-    ) * 100::numeric AS asignado
+    ) * 100 AS asignado
    FROM core_ingreso periodo WHERE anio = ANY( %s )
   GROUP BY periodo.anio
--- core/charts/aci_municipio.sql
+++ core/charts/aci_municipio.sql
@@ -4,14 +4,14 @@ SELECT ROW_NUMBER() OVER(ORDER BY anio) AS id, periodo.anio,
         ( SELECT sum(core_ingresodetalle.ejecutado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
              JOIN lugar_municipio ON core_ingreso.municipio_id = lugar_municipio.id
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0 AND lugar_municipio.slug = %s)
         - 
         ( SELECT sum(core_gastodetalle.ejecutado) AS sum
            FROM core_gastodetalle
              JOIN core_gasto ON core_gastodetalle.gasto_id = core_gasto.id
-             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id::text = core_tipogasto.codigo::text
+             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id = core_tipogasto.codigo
              JOIN lugar_municipio ON core_gasto.municipio_id = lugar_municipio.id
           WHERE core_gasto.anio = periodo.anio AND core_tipogasto.clasificacion = 0 AND lugar_municipio.slug = %s)
         )
@@ -19,23 +19,23 @@ SELECT ROW_NUMBER() OVER(ORDER BY anio) AS id, periodo.anio,
         NULLIF( (SELECT sum(core_ingresodetalle.ejecutado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
              JOIN lugar_municipio ON core_ingreso.municipio_id = lugar_municipio.id
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0 AND lugar_municipio.slug = %s), 0)
-    ) * 100::numeric AS ejecutado,
+    ) * 100 AS ejecutado,
     (
         (
         ( SELECT sum(core_ingresodetalle.asignado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
              JOIN lugar_municipio ON core_ingreso.municipio_id = lugar_municipio.id
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0 AND lugar_municipio.slug = %s)
         - 
         ( SELECT sum(core_gastodetalle.asignado) AS sum
            FROM core_gastodetalle
              JOIN core_gasto ON core_gastodetalle.gasto_id = core_gasto.id
-             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id::text = core_tipogasto.codigo::text
+             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id = core_tipogasto.codigo
              JOIN lugar_municipio ON core_gasto.municipio_id = lugar_municipio.id
           WHERE core_gasto.anio = periodo.anio AND core_tipogasto.clasificacion = 0 AND lugar_municipio.slug = %s)
         )
@@ -43,9 +43,9 @@ SELECT ROW_NUMBER() OVER(ORDER BY anio) AS id, periodo.anio,
         NULLIF( (SELECT sum(core_ingresodetalle.asignado) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
-             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id::text = core_tipoingreso.codigo::text
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
              JOIN lugar_municipio ON core_ingreso.municipio_id = lugar_municipio.id
           WHERE core_ingreso.anio = periodo.anio AND core_tipoingreso.clasificacion = 0 AND lugar_municipio.slug = %s), 0)
-    ) * 100::numeric AS asignado
+    ) * 100 AS asignado
    FROM core_ingreso periodo WHERE anio = ANY( %s )
   GROUP BY periodo.anio
