c8eca416f108e0b8ed2e40a6d71e792c0b4912f9
Alvarez Velez, Juan Carlos <alvarezvelez.juancarlos@gmail.com>
2017-02-21 04:56:28 -0600

    maps, svg icons, and more

---
+++ core/static/js/cofog.js
@@ -0,0 +1,21 @@
+/*
+ * BubbleTree Style for COFOG taxonomy
+ *
+ */
+ 
+if (!BubbleTree) window.alert('You must include the BubbleTree before including the styles');
+
+BubbleTree.Styles = BubbleTree.Styles || {};
+
+BubbleTree.Styles.Cofog = {
+	'01': { icon: 'static/styles/icons/recaudacion.svg', color: '#C75746' },
+	'02': { icon: 'static/styles/icons/transferencias.svg', color: '#0AB971' },
+	'03': { icon: 'static/styles/icons/gobierno-centralv2.svg', color: '#EC2406' }, 
+	'04': { icon: 'static/styles/icons/donaciones.svg', color: '#790586' }, 
+	'05': { icon: 'static/styles/icons/prestamosv4.svg', color: '#2A3A03' },	
+	'06': { icon: 'static/styles/icons/our-streets.svg', color: '#D33673' },		
+	'07': { icon: 'static/styles/icons/health.svg', color: '#4E6D00' },
+	'08': { icon: 'static/styles/icons/culture.svg', color: '#938626' },					
+	'09': { icon: 'static/styles/icons/education.svg' },				
+	'10': { icon: 'static/styles/icons/helping-others.svg', color: '#935B3B' }
+};
