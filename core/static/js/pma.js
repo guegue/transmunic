992b27b010711cffd61d0620653b5ebcf929f62b
Juan C. Álvarez <alvarezvelez.juancarlos@gmail.com>
2017-05-17 23:59:27 -0600

    enable aternateive chart views

--- core/static/js/pma.js
+++ core/static/js/pma.js
@@ -1,6 +1,12 @@
 $(function() {
+    $("#chart-pie-1").click(function(){
+        $(".chart-container [class*=' view-']").toggleClass( "view-main" )
+        $("#bubble-1").hide("slow");
+        $("#ejecutado").show("slow");
+    });
     $("#chart-bubble-1").click(function(){
         $(".chart-container [class*=' view-']").toggleClass( "view-main" )
-        $("#ejecutado").toggleClass( "view-alternative" ).toggleClass( "view-main" )
+        $("#ejecutado").hide("slow");
+        $("#bubble-1").show("slow");
     });
-})
+});
