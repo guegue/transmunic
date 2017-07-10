ea7183290a25578b184418ac2d0a90cb5092ee2c
Juan C. Álvarez <alvarezvelez.juancarlos@gmail.com>
2017-07-10 03:39:21 -0600

    track migrations

---
+++ core/migrations/0003_auto_20170708_2218.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0002_catinversion_color'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='catinversion',
+            name='shortname',
+            field=models.CharField(max_length=25, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
