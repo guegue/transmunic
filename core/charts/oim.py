0faf07ef26a4c7b0991d77615625446cb479619f
stickler-ci <support@stickler-ci.com>
2019-11-14 20:39:59 +0000

    Fixing style errors.

--- core/charts/oim.py
+++ core/charts/oim.py
@@ -794,13 +794,12 @@ def oim_chart(municipio=None, year=None, portada=False):
             periodo = PERIODO_FINAL
             quesumar = 'ejecutado'
             value = IngresoDetalle.objects.\
-                        filter(ingreso__anio=year,
-                                ingreso__periodo=periodo,
-                                subsubtipoingreso__origen__nombre=label,
-                                ingreso__municipio__clasificaciones__clasificacion=
-                                mi_clase.clasificacion,
-                                ingreso__municipio__clase__anio=year).\
-                                aggregate(total=Sum(quesumar))['total']
+                filter(ingreso__anio=year,
+                       ingreso__periodo=periodo,
+                       subsubtipoingreso__origen__nombre=label,
+                       ingreso__municipio__clasificaciones__clasificacion=mi_clase.clasificacion,
+                       ingreso__municipio__clase__anio=year).\
+                aggregate(total=Sum(quesumar))['total']
             if value:
                 value = value / mi_clase_count
             porano_table[label]['extra'] = value if value else '...'
