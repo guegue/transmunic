f767a8775eebe5d78f84007a520329084896a6da
Javier Wilson <javier.wilson@gmail.com>
2019-11-14 20:10:50 -0600

    smarter deduper

--- core/models.py
+++ core/models.py
@@ -48,14 +48,15 @@ class Profile(models.Model):
 
 
 class Organizacion(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)
     descripcion = models.TextField(blank=True, null=True)
     correo = models.CharField(max_length=100, null=True, blank=True)
     web = models.CharField(max_length=200, null=True, blank=True)
     logo = ImageField(upload_to='organizacion', null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = 'Organizaciones'
+        verbose_name_plural = u'Organizaciones'
+        verbose_name = u'Organización'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -64,14 +65,14 @@ class Organizacion(models.Model):
 
 class Grafico(models.Model):
     id = models.CharField(max_length=25,  primary_key=True)
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)
     descripcion = models.TextField(blank=True, null=True)
     notas = models.TextField(blank=True, null=True)
     imagen_objetivo = ImageField(upload_to='grafico', null=True, blank=True)
     imagen_actual = ImageField(upload_to='grafico', null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = 'Graficos'
+        verbose_name = u'Gráfico'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -79,18 +80,21 @@ class Grafico(models.Model):
 
 
 class Anio(models.Model):
-    anio = models.IntegerField()
+    anio = models.IntegerField(unique=True)
     periodo = models.CharField(max_length=1)
     inicial = models.DateField(null=True, blank=True)
     actualizado = models.DateField(null=True, blank=True)
     final = models.DateField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = u'Año'
 
     def __unicode__(self):
         return u'%s %s' % (self.anio, self.periodo)
 
 
 class CatInversion(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=80)
     minimo = models.DecimalField(max_digits=5, decimal_places=2, null=True,
                                  blank=True)
@@ -99,8 +103,8 @@ class CatInversion(models.Model):
     shortname = models.CharField(max_length=25, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Categorias de inversion'
-        verbose_name = 'Categoria de inversion'
+        verbose_name_plural = u'Categorías de inversión'
+        verbose_name = u'Categoría de inversión'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -108,12 +112,13 @@ class CatInversion(models.Model):
 
 
 class OrigenGasto(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)
     slug = AutoSlugField(populate_from='nombre')
     shortname = models.CharField(max_length=25, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Origen  de los gastos'
+        verbose_name_plural = 'Origenes de los gastos'
+        verbose_name = 'Origen de los gastos'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -140,7 +145,8 @@ class TipoGasto(models.Model):
         default=0, null=True)
 
     class Meta:
-        verbose_name_plural = 'Tipo de gastos'
+        verbose_name_plural = 'Tipos de gastos'
+        verbose_name = 'Tipo de gastos'
         ordering = ['codigo']
 
     def __unicode__(self):
@@ -155,7 +161,8 @@ class SubTipoGasto(models.Model):
     shortname = models.CharField(max_length=25, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Sub-Tipo de gastos'
+        verbose_name_plural = 'Sub tipos de gastos'
+        verbose_name = 'Sub tipo de gastos'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -171,7 +178,8 @@ class SubSubTipoGasto(models.Model):
     origen = models.ForeignKey(OrigenGasto, related_name='origen', null=True)
 
     class Meta:
-        verbose_name_plural = 'Sub-Sub-Tipo de gastos'
+        verbose_name_plural = 'Sub sub tipos de gastos'
+        verbose_name = 'Sub sub tipo de gastos'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -181,12 +189,13 @@ class SubSubTipoGasto(models.Model):
 class OrigenRecurso(models.Model):
     # FIXME: id es auto!!! RECAUDACION puede no ser == 1
     RECAUDACION = 1
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)
     slug = AutoSlugField(populate_from='nombre')
-    shortname = models.CharField(max_length=25, blank=True, null=True)
+    shortname = models.CharField(max_length=25, blank=True, null=True, unique=True)
 
     class Meta:
-        verbose_name_plural = 'Origen  de los recursos'
+        verbose_name_plural = 'Origenes de los recursos'
+        verbose_name = 'Origen de los recursos'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -210,7 +219,8 @@ class TipoIngreso(models.Model):
         choices=CLASIFICACION_CHOICES, default=0, null=True)
 
     class Meta:
-        verbose_name_plural = 'Tipo de ingreso'
+        verbose_name_plural = 'Tipos de ingreso'
+        verbose_name = 'Tipo de ingreso'
         ordering = ['codigo']
 
     def __unicode__(self):
@@ -225,7 +235,8 @@ class SubTipoIngreso(models.Model):
     shortname = models.CharField(max_length=25, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Subtipos de ingresos'
+        verbose_name_plural = 'Subtipos de ingreso'
+        verbose_name = 'Subtipo de ingreso'
         ordering = ['codigo']
 
     def __unicode__(self):
@@ -241,7 +252,8 @@ class SubSubTipoIngreso(models.Model):
     origen = models.ForeignKey(OrigenRecurso, related_name='origen', null=True)
 
     class Meta:
-        verbose_name_plural = 'Sub-subtipos de ingresos'
+        verbose_name_plural = 'Sub-subtipos de ingreso'
+        verbose_name = 'Sub-subtipo de ingreso'
         ordering = ['codigo']
 
     def __unicode__(self):
@@ -259,13 +271,17 @@ class Ingreso(models.Model):
         chained_model_field='depto', null=True, blank=True)
     descripcion = models.TextField(blank=True, null=True)
 
+    class Meta:
+        unique_together = [['anio', 'periodo', 'municipio']]
+        ordering = ['anio', 'periodo', 'municipio']
+
+    def __unicode__(self):
+        return u"{}:{}:{}".format(self.anio, self.periodo, self.municipio)
+
     def save(self, *args, **kwargs):
         if not self.departamento_id and self.municipio_id:
             self.departamento_id = self.municipio.depto_id
         super(Ingreso, self).save(*args, **kwargs)
-
-    class Meta:
-        verbose_name_plural = 'Ingresos'
 
 
 class IngresoDetalle(models.Model):
@@ -286,11 +302,12 @@ class IngresoDetalle(models.Model):
 
     class Meta:
         unique_together = [['ingreso', 'codigo']]
-        verbose_name_plural = 'Detalle de ingresos'
-        ordering = ['ingreso']
+        ordering = ['ingreso', 'codigo']
+        verbose_name_plural = 'Detalles de ingresos'
+        verbose_name = 'Detalle de ingresos'
 
     def __unicode__(self):
-        return self.codigo
+        return u"{}:{}:{}".format(self.id, self.codigo, self.cuenta)
 
 
 class Gasto(models.Model):
@@ -304,9 +321,11 @@ class Gasto(models.Model):
     descripcion = models.TextField(blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Gastos'
-        ordering = ['fecha']
+        unique_together = [['anio', 'periodo', 'municipio']]
+        ordering = ['anio', 'periodo', 'municipio']
 
+    def __unicode__(self):
+        return u"{}:{}:{}".format(self.anio, self.periodo, self.municipio)
 
 # detalle del gasto
 class GastoDetalle(models.Model):
@@ -327,19 +346,21 @@ class GastoDetalle(models.Model):
 
     class Meta:
         unique_together = [['gasto', 'codigo']]
-        verbose_name_plural = 'Detalle de gastos'
-        ordering = ['gasto']
+        verbose_name_plural = 'Detalles de gastos'
+        verbose_name = 'Detalle de gastos'
+        ordering = ['gasto', 'codigo']
 
     def __unicode__(self):
-        return self.codigo
+        return u"{}:{}:{}".format(self.id, self.codigo, self.cuenta)
 
 
 class TipoProyecto(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)
     slug = AutoSlugField(populate_from='nombre')
 
     class Meta:
-        verbose_name_plural = 'Tipo de proyectos'
+        verbose_name_plural = 'Tipos de proyectos'
+        verbose_name = 'Tipo de proyectos'
         ordering = ['nombre']
 
     def __unicode__(self):
@@ -355,7 +376,13 @@ class Inversion(models.Model):
     periodo = models.CharField(max_length=1, null=False)
 
     class Meta:
-        verbose_name_plural = 'Inversion'
+        unique_together = [['anio', 'periodo', 'municipio']]
+        verbose_name_plural = u'Inversiones'
+        verbose_name = u'Inversión'
+        ordering = ['anio', 'periodo', 'municipio']
+
+    def __unicode__(self):
+        return u"{}:{}:{}".format(self.anio, self.periodo, self.municipio)
 
 
 class Proyecto(models.Model):
@@ -397,18 +424,19 @@ class Proyecto(models.Model):
 
     class Meta:
         unique_together = [['inversion', 'codigo']]
-        verbose_name_plural = 'Proyectos'
+        ordering = ['inversion', 'codigo']
 
     def __unicode__(self):
-        return self.nombre
+        return u"{}:{}:{}".format(self.id, self.codigo, self.nombre)
 
 
 # financiamiento
 class TipoFuenteFmto(models.Model):
-    nombre = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=250, unique=True)
     slug = AutoSlugField(populate_from='nombre')
 
     class Meta:
+        verbose_name = 'Tipo de Fuente de financiamiento'
         verbose_name_plural = 'Tipos de Fuentes de financiamiento'
         ordering = ['nombre']
 
@@ -417,11 +445,12 @@ class TipoFuenteFmto(models.Model):
 
 
 class FuenteFmto(models.Model):
-    nombre = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=250, unique=True)
     slug = AutoSlugField(populate_from='nombre')
     tipofuente = models.ForeignKey(TipoFuenteFmto)
 
     class Meta:
+        verbose_name = 'Fuente de financiamiento'
         verbose_name_plural = 'Fuentes de financiamiento'
         ordering = ['nombre']
 
@@ -441,7 +470,9 @@ class InversionFuente(models.Model):
             null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = 'Inversion de fuentes de financiamiento'
+        verbose_name = u'Inversión de fuentes de financiamiento'
+        verbose_name_plural = u'Inversiones de fuentes de financiamiento'
+        ordering = ['anio', 'periodo', 'municipio']
 
 
 class InversionFuenteDetalle(models.Model):
@@ -456,6 +487,7 @@ class InversionFuenteDetalle(models.Model):
             max_digits=12, decimal_places=2, blank=False, null=False)
 
     class Meta:
-        verbose_name_plural = 'Detalle de inversion por fuente'
-        ordering = ['inversionfuente']
+        verbose_name = u'Detalle de inversión por fuente'
+        verbose_name_plural = u'Detalles de inversión por fuente'
+        ordering = ['inversionfuente', 'tipofuente', 'fuente']
 
