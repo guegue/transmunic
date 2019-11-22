619639ce3c579b00f5c35bebd8c7c06c5f1d4858
Javier Wilson <javier.wilson@gmail.com>
2019-11-22 01:39:31 -0600

    remoev star import

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from core.models import *
+from core.models import (Profile, Organizacion, Anio, Grafico, CatInversion, TipoGasto,
+        SubTipoGasto, SubSubTipoGasto, OrigenRecurso, OrigenGasto, TipoIngreso, SubSubTipoIngreso,
+        SubTipoIngreso,TipoFuenteFmto, FuenteFmto, InversionFuente,Inversion,Proyecto, Ingreso,
+        Gasto, IngresoRenglon, GastoRenglon, GastoDetalle, IngresoDetalle, InversionFuenteDetalle)
 
 # Register your models here.
 class SubSubTipoIngresoInline(admin.TabularInline):
