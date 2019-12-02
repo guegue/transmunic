0989979992768d22f8f925c382f62df38bd2b58d
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2019-12-02 15:52:54 -0600

    aplicando recomendaciones de stickler en forms.py y importer.py

--- core/importer.py
+++ core/importer.py
@@ -27,8 +27,11 @@ def import_file(excel_file, municipio, year, periodo, start_row, end_row, table)
     book = load_workbook(filename=excel_file)
     sheet = book.active
     today = date.today()
-    main_object, created = t['main'].objects.get_or_create(municipio=municipio, anio=year,
-                                                           periodo=periodo, defaults={'fecha': today})
+    main_object, created = t['main'].objects.\
+                    get_or_create(municipio=municipio,
+                                  anio=year,
+                                  periodo=periodo,
+                                  defaults={'fecha': today})
 
     for row in sheet[start_row:end_row]:
         joined = unicode(row[0].value)
@@ -119,8 +122,8 @@ class ReglonIngresosView(LoginRequiredMixin, FormView):
     form = RenglonIngresoForm
 
     def form_valid(self, form):
-        data = form.cleaned_data
-        # get the data from the form
+        form.cleaned_data
+        #get the data from the form
         renglon_codigo = self.request.POST.getlist('renglon[codigo]')
         municipio = self.request.POST.get('municipio')
         anio = self.request.POST.get('year')
