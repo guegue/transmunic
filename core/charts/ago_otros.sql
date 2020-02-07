02ad0b08a5c04551340c5a20ab1a5ce2b91d5afc
Maribel Fonseca <maribel.fonseca@guegue.net>
2020-02-07 15:44:08 -0600

    Corrigiendo formular de AGO

--- core/charts/ago_otros.sql
+++ core/charts/ago_otros.sql
@@ -1,17 +1,23 @@
 SELECT muni.nombre, (
-        ( SELECT SUM(core_ingresodetalle.{quesumar}) AS sum
+        (( SELECT SUM(core_ingresodetalle.{quesumar}) AS sum
            FROM core_ingresodetalle
              JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
              JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
           WHERE core_ingreso.anio = {year} AND core_Ingreso.periodo='{periodo}' AND core_tipoingreso.clasificacion = {tipoingreso} AND core_tipoingreso.codigo <> '{notipoingreso}' AND core_Ingreso.municipio_id=muni.id)
-        /
-        NULLIF( (SELECT SUM(core_gastodetalle.{quesumar}) AS sum
+        -
+        (SELECT SUM(core_gastodetalle.{quesumar}) AS sum
            FROM core_gastodetalle
              JOIN core_gasto ON core_gastodetalle.gasto_id = core_gasto.id
-             JOIN core_tipogasto ON core_gastodetalle.tipogasto_id = core_tipogasto.codigo
-          WHERE core_gasto.anio = {year} AND core_Gasto.periodo='{periodo}' AND core_tipogasto.clasificacion = {tipoingreso} AND core_Gasto.municipio_id=muni.id)
-        , 0)
-    -1 ) * 100 AS {quesumar}  FROM lugar_municipio AS muni 
+             JOIN core_subsubtipogasto ON core_gastodetalle.subsubtipogasto_id = core_subsubtipogasto.codigo
+          WHERE core_gasto.anio = {year} AND core_Gasto.periodo='{periodo}' AND core_subsubtipogasto.clasificacion = {tipoingreso} AND core_Gasto.municipio_id=muni.id)
+	)
+	/
+        NULLIF(( SELECT SUM(core_ingresodetalle.{quesumar}) AS sum
+           FROM core_ingresodetalle
+             JOIN core_ingreso ON core_ingresodetalle.ingreso_id = core_ingreso.id
+             JOIN core_tipoingreso ON core_ingresodetalle.tipoingreso_id = core_tipoingreso.codigo
+          WHERE core_ingreso.anio = {year} AND core_Ingreso.periodo='{periodo}' AND core_tipoingreso.clasificacion = {tipoingreso} AND core_tipoingreso.codigo <> '{notipoingreso}' AND core_Ingreso.municipio_id=muni.id), 0)
+    ) * 100 AS {quesumar}  FROM lugar_municipio AS muni 
              JOIN lugar_clasificacionmunicano ON muni.id=lugar_clasificacionmunicano.municipio_id AND
          lugar_clasificacionmunicano.anio = {year}
 WHERE lugar_clasificacionmunicano.clasificacion_id={mi_clase}
