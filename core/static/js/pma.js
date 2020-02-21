0c85c0e8cadc5dbd39a71528bf8a4c326ebbf7ed
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-02-21 09:16:01 -0600

    migrando evento change de select municipio2 a pma.js

--- core/static/js/pma.js
+++ core/static/js/pma.js
@@ -39,30 +39,44 @@ $(function () {
     $("#show-detail-3").click(function () {
         $("#detail-3").toggle("slow");
     });
-    $('body').on('submit', '#indicatorfilter', function (e) {
-        e.preventDefault();
-        let inputs_datos = $(this).serializeArray();
-        let indicador = inputs_datos.find((obj) => {
-            return (obj.name.toLowerCase() === 'indicador');
-        });
-
-        //NOTE: eliminando year de arreglo si indicador es transferencias
-        if (indicador.value.includes('/transferencias')) {
-            inputs_datos = inputs_datos.filter((obj) => {
-                return (obj.name.toLowerCase() !== 'year');
+    $('body')
+        .on('submit', '#indicatorfilter', function (e) {
+            e.preventDefault();
+            let inputs_datos = $(this).serializeArray();
+            let indicador = inputs_datos.find((obj) => {
+                return (obj.name.toLowerCase() === 'indicador');
             });
-        }
 
-        //NOTE: eliminando indicador de arreglo
-        inputs_datos = inputs_datos.filter((obj) => {
-            return (obj.name !== indicador.name);
-        });
-        let url_indicador = indicador.value;
-        //NOTE: crear parametros url
-        let parameters = '?' + inputs_datos.map((parameter) => {
-            if (parameter.value)
-                return `${parameter.name}=${parameter.value}`;
-        }, []).join('&');
-        window.location.href = `${url_indicador}${parameters}`;
-    });
+            //NOTE: eliminando year de arreglo si indicador es transferencias
+            if (indicador.value.includes('/transferencias')) {
+                inputs_datos = inputs_datos.filter((obj) => {
+                    return (obj.name.toLowerCase() !== 'year');
+                });
+            }
+
+            //NOTE: eliminando indicador de arreglo
+            inputs_datos = inputs_datos.filter((obj) => {
+                return (obj.name !== indicador.name);
+            });
+            let url_indicador = indicador.value;
+            //NOTE: crear parametros url
+            let parameters = '?' + inputs_datos.map((parameter) => {
+                if (parameter.value)
+                    return `${parameter.name}=${parameter.value}`;
+            }, []).join('&');
+            window.location.href = `${url_indicador}${parameters}`;
+        })
+        .on('change', '#municipio2', function () {
+            let parameters = window.location.search;
+            let current_url = window.location.pathname;
+            const urlParams = new URLSearchParams(parameters);
+            //valido si existe municipio2 en la url para agregar o actualizar su valor
+            if (urlParams.has('municipio2')) {
+                urlParams.set('municipio2', $(this).val())
+            } else {
+                urlParams.append('municipio2', $(this).val())
+            }
+
+            window.location.href = `${current_url}?${urlParams.toString()}`;
+        })
 });
