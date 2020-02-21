b9152a584863f7d496a1446a0496031bb1a1e8ad
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-02-21 09:12:47 -0600

    identando codigo de pma.js

--- core/static/js/pma.js
+++ core/static/js/pma.js
@@ -1,25 +1,25 @@
-$(function() {
-    $(".toggle-chart").click(function() {
+$(function () {
+    $(".toggle-chart").click(function () {
         chartcontainer = $(this).data('chartcontainer');
         var charts = $("#" + chartcontainer).closest(".col-md-7").find(".view-main");
-        $(charts).each(function(){
+        $(charts).each(function () {
             $(this).hide("slow").removeClass("view-main").addClass("view-alternative");
         });
 
-            $("#" + chartcontainer).show("slow", function(chartcontainer){
-            var width = $( window ).width();
-            var height = $( window ).height();
+        $("#" + chartcontainer).show("slow", function (chartcontainer) {
+            var width = $(window).width();
+            var height = $(window).height();
             console.log(width + "x" + height);
-            if(!$(this).hasClass("bubbletree-wrapper")){
-                if (width < 540){
-                    $(this).highcharts().setSize(Math.round(width*0.8), width);
-                }else if(width >= 540 && width < 720){
+            if (!$(this).hasClass("bubbletree-wrapper")) {
+                if (width < 540) {
+                    $(this).highcharts().setSize(Math.round(width * 0.8), width);
+                } else if (width >= 540 && width < 720) {
                     $(this).highcharts().setSize(432, 324);
-                }else if(width >= 720 && width < 960){
+                } else if (width >= 720 && width < 960) {
                     $(this).highcharts().setSize(540, 405);
-                }else if(width >= 960 && width < 1140){
+                } else if (width >= 960 && width < 1140) {
                     $(this).highcharts().setSize(600, 450);
-                }else{
+                } else {
                     $(this).highcharts().setSize(600, 450);
                 }
             }
@@ -28,26 +28,26 @@ $(function() {
 
     });
 
-    $("#show-detail-1").click(function() {
+    $("#show-detail-1").click(function () {
         $("#detail-1").toggle("slow");
     });
 
-    $("#show-detail-2").click(function() {
+    $("#show-detail-2").click(function () {
         $("#detail-2").toggle("slow");
     });
 
-    $("#show-detail-3").click(function() {
+    $("#show-detail-3").click(function () {
         $("#detail-3").toggle("slow");
     });
-     $('body').on('submit', '#indicatorfilter', function (e) {
+    $('body').on('submit', '#indicatorfilter', function (e) {
         e.preventDefault();
         let inputs_datos = $(this).serializeArray();
         let indicador = inputs_datos.find((obj) => {
             return (obj.name.toLowerCase() === 'indicador');
         });
 
-         //NOTE: eliminando year de arreglo si indicador es transferencias
-        if (indicador.value.includes('/transferencias')){
+        //NOTE: eliminando year de arreglo si indicador es transferencias
+        if (indicador.value.includes('/transferencias')) {
             inputs_datos = inputs_datos.filter((obj) => {
                 return (obj.name.toLowerCase() !== 'year');
             });
