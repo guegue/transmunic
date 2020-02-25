e7f74e6bc9e25a9acc4d6f8ae8fb4dc84c89b213
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-02-25 01:19:29 -0600

    validando si existe un registro en gasto con la combinacion de municipio,año y periodo sino retornar mensaje de que municipio ya cuenta con registros

--- core/importer.py
+++ core/importer.py
@@ -373,44 +373,58 @@ class RenglonGastosView(LoginRequiredMixin, FormView):
         # obteniendo detalles del municipio seleccionado
         municipio = Municipio.objects.filter(id=municipio).first()
 
-        # insertando en core_gasto
-        gasto = Gasto()
-        gasto.fecha = date.today()
-        gasto.departamento_id = municipio.depto_id
-        gasto.municipio_id = municipio.id
-        gasto.anio = anio
-        gasto.periodo = periodo
-        gasto.save()
+        # validando si existe un gasto en el sistema que este relacionado
+        # a el municipio, en periodo del año indicados desdel formulario
+        gasto = Gasto.objects.filter(municipio_id=municipio.id,
+                                     anio=anio,
+                                     periodo=periodo).first()
 
-        # insertando en core_ingresodetalle
-        for codigo in renglon_codigo:
-            # obteniendo el asignado y ejecutado de un renglon
-            asignado = self.request.POST.get('renglon_asignado[{}]'.format(codigo))
-            ejecutado = self.request.POST.get('renglon_ejecutado[{}]'.format(codigo))
-            asignado = xnumber(asignado)
-            ejecutado = xnumber(ejecutado)
+        if not gasto:
+            # insertando en core_gasto
+            gasto = Gasto()
+            gasto.fecha = date.today()
+            gasto.departamento_id = municipio.depto_id
+            gasto.municipio_id = municipio.id
+            gasto.anio = anio
+            gasto.periodo = periodo
+            gasto.save()
 
-            if asignado > 0 and ejecutado >= 0:
-                subsubtipo = SubSubTipoGasto.objects. \
-                    filter(gastorenglon__codigo='11100'). \
-                    values(id=F('codigo'),
-                           id_subtipo=F('subtipogasto_id'),
-                           id_tipo=F('subtipogasto__tipogasto_id')). \
-                    first()
+            # insertando en core_ingresodetalle
+            for codigo in renglon_codigo:
+                # obteniendo el asignado y ejecutado de un renglon
+                asignado = self.request.POST.get('renglon_asignado[{}]'.format(codigo))
+                ejecutado = self.request.POST.get('renglon_ejecutado[{}]'.format(codigo))
+                asignado = xnumber(asignado)
+                ejecutado = xnumber(ejecutado)
 
-                # guardando el detalle de cada renglon
-                gasto_detalle = GastoDetalle()
-                gasto_detalle.codigo_id = codigo
-                gasto_detalle.asignado = asignado
-                gasto_detalle.ejecutado = ejecutado
-                gasto_detalle.gasto_id = gasto.id
-                gasto_detalle.subsubtipogasto_id = subsubtipo.get('id')
-                gasto_detalle.subtipogasto_id = subsubtipo.get('id_subtipo')
-                gasto_detalle.tipogasto_id = subsubtipo.get('id_tipo')
-                gasto_detalle.cuenta = 'SA'
-                gasto_detalle.save()
+                if asignado > 0 and ejecutado >= 0:
+                    subsubtipo = SubSubTipoGasto.objects. \
+                        filter(gastorenglon__codigo='11100'). \
+                        values(id=F('codigo'),
+                               id_subtipo=F('subtipogasto_id'),
+                               id_tipo=F('subtipogasto__tipogasto_id')). \
+                        first()
 
-        return super(RenglonGastosView, self).form_valid(form)
+                    # guardando el detalle de cada renglon
+                    gasto_detalle = GastoDetalle()
+                    gasto_detalle.codigo_id = codigo
+                    gasto_detalle.asignado = asignado
+                    gasto_detalle.ejecutado = ejecutado
+                    gasto_detalle.gasto_id = gasto.id
+                    gasto_detalle.subsubtipogasto_id = subsubtipo.get('id')
+                    gasto_detalle.subtipogasto_id = subsubtipo.get('id_subtipo')
+                    gasto_detalle.tipogasto_id = subsubtipo.get('id_tipo')
+                    gasto_detalle.cuenta = 'SA'
+                    gasto_detalle.save()
+
+            return super(RenglonGastosView, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR, 'El municipio ya cuenta con un gasto para el'
+                                                           ' año seleccionado en el periodo indicado')
+        return super(RenglonGastosView, self).form_invalid(form)
 
     def get_success_url(self):
         messages.add_message(self.request, messages.INFO, 'Guardado Exitosamente')
