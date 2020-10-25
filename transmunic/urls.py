76eab444a5dd47a64439ad1a9c480d2b85eb79d7
javier wison <javier.wilson@viamo.io>
2020-10-25 03:47:38 -0600

    first steps for django3

--- transmunic/urls.py
+++ transmunic/urls.py
@@ -1,17 +1,33 @@
-from django.conf.urls import url, include
-from django.conf.urls.static import static
+"""transmunic URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import url, path, include
+from django.urls.static import static
 from django.conf import settings
+
 from django.contrib import admin
+from django.urls import path
 
 from website.views import DocumentoTipoListView
 from core.views import home, municipio
 
-admin.autodiscover()
-
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^core/', include('core.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
     url(r'^chaining/', include('pixelfields_smart_selects.urls')),
     url(r'^resumen-municipal', municipio, name='budget-summary'),
     url(r'^resumen-municipal/(?P<year>[0-9]{4})/$', municipio, name='anual-budget-summary'),
